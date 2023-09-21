019561c1cd9a91aad2dbfd43268ae7f8d46167a9
Tarho <115691708+Tarho@users.noreply.github.com>
2023-09-21 07:15:04 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={images} alt="images" width="500" height="500" />
-        <p>
-         Welcome to my page
-        </p>
-        {/*https://www.youtube.com/watch?v=p5fnHWznTBQ&t=2496s*/}
-        <a href="https://www.facebook.com/vux.1309">Click here to view my info</a>
-          <h2>This is my song. Let's enjoy it.</h2>
-          <iframe width="500" height="500" src="https://www.youtube.com/embed/MoN9ql6Yymw"/>
+        <h1>Hello World</h1>
       </header>
     </div>
   );
